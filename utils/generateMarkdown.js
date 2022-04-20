917b8366e52a3dac305fd942d3991388dc5a3763
rich p <richardferry3@gmail.com>
2022-04-20 01:39:00 -0400

    add layout markdown for generateMarkDown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,28 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+## Description 
+${data.description}
+# Table of Contents
+1. [Instructions](#Instructions)
+2. [Usage](#Usage)
+3. [License](#License)
+4. [Contributions](#Contributions)
+5. [Test](#Test)
+6. [Questions](#questions)
+## Instructions
+${data.instructions}
+## Usage
+${data.usage}
+## License
+* ${data.license}
+## Contributions
+${data.contributions}
+## Test
+${data.test}
+### Questions
+* ${data.email}
+* ${data.github}
 `;
 }
 
