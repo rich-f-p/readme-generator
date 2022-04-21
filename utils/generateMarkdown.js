a77fbaab87e2766e55afee868db778d523cde8e7
rich p <richardferry3@gmail.com>
2022-04-20 20:34:04 -0700

    add function to render license badge and place the badge info into the template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  var badge = "" ;
+  switch(license){
+case "GPL":
+  badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  break;
+case "APACHE":
+  badge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  break;
+case "MIT":
+  badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  break;
+case "none":
+  badge = "";
+  break;
+}return badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -12,7 +27,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}   ${renderLicenseBadge(data.license)}
 ## Description 
 ${data.description}
 
@@ -41,11 +56,5 @@ For questions proceed to:
 * [${data.github}](${data.github})
 `;
 }
-// // gpl
-// "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
-// license = "";
-// //apache
-// "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-//  //mit
-//  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+
 module.exports = generateMarkdown;
